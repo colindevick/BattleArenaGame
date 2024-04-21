8f3e99602f5034433d671b3177768245e432ec40
Colin <63689317+colindevick@users.noreply.github.com>
2024-04-20 20:43:13 -0500

    Initialize repository with already created files

---
+++ BattleArenaGame/src/server/InitializePlayer.lua
@@ -0,0 +1,13 @@
+--On player connect, user attibutes are set.
+local ServerScriptService = game:GetService("ServerScriptService")
+local Players = game:GetService("Players")
+local attributeSetter = require(ServerScriptService.AttributeSetter)
+
+
+
+
+function initalizeAttributes(player)
+	attributeSetter.SetInitalattibutes(player)
+end
+
+Players.PlayerAdded:Connect(initalizeAttributes)
